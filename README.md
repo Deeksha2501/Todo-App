cad7c26c1abef067c0ebbd95cf9a3dcc86675bdc
Deeksha Sharma <sharmadeeksha325@gmail.com>
2020-06-02 15:37:41 +0530

    updated🚀

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Todo-App
 A basic todo application implemented using firebase.
 
+![screenshot of project](ss.png)
---
+++ apikey.js
@@ -0,0 +1,10 @@
+var config = {
+    apiKey: "AIzaSyAIeLvUXK6v34DpYneFHDUNnAjs2YRVx2Q",
+    authDomain: "check-f93fb.firebaseapp.com",
+    databaseURL: "https://check-f93fb.firebaseio.com",
+    projectId: "check-f93fb",
+    storageBucket: "check-f93fb.appspot.com",
+    messagingSenderId: "679908346776",
+    appId: "1:679908346776:web:9ab8978a4aee07921ab9ed",
+    measurementId: "G-5F4HD48LMG"
+};
