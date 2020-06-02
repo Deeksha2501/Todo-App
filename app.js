bf0830d5c57a7b70555c370eabd9cc943c88d406
Deeksha Sharma <sharmadeeksha325@gmail.com>
2020-06-02 15:26:37 +0530

    making it better

--- app.js
+++ app.js
@@ -5,30 +5,32 @@ var form = document.getElementById('add-cafe-form');
 
 //create an element and render cafe
 function renderCafe(doc){
-    // var checkbox = document.createElement('input');
-    // checkbox.type = "checkbox";
-    // checkbox.id = "checkbox";
-    // checkbox.name = "check";
-    // var lab = document.createElement('label');
-    // lab.for = "check";
-    var html = ` <input type="checkbox" name="check" id="${doc.id}">
-    <label for="${doc.id}"><div>&#10004;</div></label>`
+    var checkbox = document.createElement('input')
+    checkbox.type = "checkbox";
+    checkbox.id =  doc.id;
+    checkbox.name = "check";
+    var html =  //<input type="checkbox" name="check" id="${doc.id}>
+    `<label for="${doc.id}"><div>&#10004;</div></label>`
+    let ID  = doc.id;
+    console.log(ID);
     let li = document.createElement('li');
-    li.id = "li_id";
     let todo = document.createElement('span');
     let motive = document.createElement('span');
     let cross = document.createElement('div');
+    let update = document.createElement('div');
     li.setAttribute('data-id' , doc.id);
     todo.textContent = doc.data().todo;
     motive.textContent = doc.data().motive;
     cross.textContent = 'Remove';
-
+    // update.textContent = '✏️';
+    update.id = 'update';
+    li.appendChild(checkbox);
     li.insertAdjacentHTML("beforeend" ,html);
     li.appendChild(todo);
     li.appendChild(motive);
+    // li.appendChild(update);
     li.appendChild(cross);
-    // li.appendChild(checkbox);
-    
+
     cafeList.appendChild(li);
 
     //deleting data
@@ -39,34 +41,39 @@ function renderCafe(doc){
         getdata();
     });
 
-    // checkbox.addEventListener('click' , e=>{
-    //     e.target.parentElement.classList.toggle('a');
-    //     // e.target.nextElementSibling.textContent = "ncjewknk"
-    // })
+    checkbox.addEventListener('click' , e=>{
+        // e.target.parentElement.classList.toggle('a');
+        // e.target.nextElementSibling.textContent = "ncjewknk"
+        console.log(checkbox.checked);
+    });
+
+
 }
 
 //getting data
-getdata = () =>{
-db.collection('cafes').get().then(snapshot => {
-    cafeList.textContent = '';
-    snapshot.docs.forEach(doc => {
-        renderCafe(doc)
-    });
-});
-}
+// getdata = () =>{
+// db.collection('cafes').get().then(snapshot => {
+//     cafeList.textContent = '';
+//     snapshot.docs.forEach(doc => {
+//         renderCafe(doc)
+//     });
+// });
+// }
 
 //saving data
 form.addEventListener('submit' , e=>{
     e.preventDefault();
     let added_todo = form.todo.value;
     let added_motive = form.motive.value;
+    let checked = false;
     if(!added_motive){
         added_motive = 'Stop all postponement';
     }
     if(added_todo){
     db.collection('cafes').add({
         todo : added_todo,
-        motive: added_motive
+        motive: added_motive,
+        checked : checked
     });
 }
     form.todo.value = '';
@@ -75,4 +82,19 @@ form.addEventListener('submit' , e=>{
     getdata();
 })
 
-getdata();
+// getdata();
+
+db.collection('cafes').onSnapshot((snapshot)=>{
+    let changes = snapshot.docChanges();
+    changes.forEach(change =>{
+        if(change.type == 'added'){
+            renderCafe(change.doc);
+        }
+        else if(change.type == 'removed'){
+            let li = document.querySelector('[data-id=' + change.doc.id + ']');
+            console.log(li);
+            cafeList.removeChild(li);
+        }
+
+    })
+})
